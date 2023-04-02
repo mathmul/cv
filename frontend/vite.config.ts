08b95b891ace7ee17c3c3cf13ddb0313d0dfa712
mathmul <mmulej@gmail.com>
2023-04-02 07:34:21 +0200

    enable .env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Checker from 'vite-plugin-checker';
 import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 let base = '/';
+console.log({ env: process.env });
 if (process.env.NODE_ENV === 'production') {
-  console.log({ env: process.env });
   base += process.env.SERVER_PATH || '';
 }
 export default defineConfig({
